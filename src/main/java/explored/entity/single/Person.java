418a1b9bfbe1261ebdc78a17e0259300017b5965
BartoszMiller <miller.bartek@gmail.com>
2016-11-03 16:30:44 +0000

    Added equals and hashCode to Person even though it is not required at this point

--- src/main/java/explored/entity/single/Person.java
+++ src/main/java/explored/entity/single/Person.java
@@ -41,4 +41,23 @@ public class Person {
     public void setSurname(String surname) {
         this.surname = surname;
     }
+
+    @Override // it is advisable not to use entity id
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (name != null ? !name.equals(person.name) : person.name != null) return false;
+        return surname != null ? surname.equals(person.surname) : person.surname == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        return result;
+    }
 }
